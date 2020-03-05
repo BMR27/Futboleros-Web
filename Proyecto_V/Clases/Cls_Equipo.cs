5f097598bf22cea2dc0f8bfcb0f8bae0d04ab409
Bryan Mejia Ruiz <bmr1919@hotmail.com>
2020-03-05 13:54:22 -0600

    Cambios

---
+++ Proyecto_V/Clases/Cls_Equipo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proyecto_V.Models;
+
+namespace Proyecto_V.Clases
+{
+    public class Cls_Equipo:Cls_Distrito
+    {
+        //INSTANCIA DE LA CLASE
+        #region INSTANCIA
+        programacion5Entities ModeloDB = new programacion5Entities();
+        #endregion
+
+
+        //ATRIBUTOS DE LA CLASE
+        #region ATRIBUTOS DE CLASE
+        public int idConsecutivo { get; set; }
+        public string NombreEquipo { get; set; }
+        public string Fundacion { get; set; }
+        #endregion
+
+
+        //CONSTRUCTORES DE LA CLASE
+        #region CONSTRUCTORES DE CLASE
+        public Cls_Equipo()
+        {
+
+        }
+        #endregion
+
+        //METODOS
+        #region METODOS DE LA CLASE
+        //METODO QUE INSERTA EL EQUIPO
+        public int pc_registrar_equipo()
+        {
+            int filas = 0;
+            try
+            {
+                filas = this.ModeloDB.SP_REGISTRA_EQUIPO(IdProvincia, IdCanton, IdDistrito, Convert.ToDateTime(Fundacion));
+            }
+            catch
+            {
+                filas = -100;
+            }
+            return filas;
+        }
+
+        #endregion
+
+
+
+
+
+    }
+}
